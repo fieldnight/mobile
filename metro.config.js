62ce87652648253503e566d18852ab4ec2008ab5
fieldnight <da.young.yun13@gmail.com>
2026-07-26 17:53:13 +0900

    chore: simplify metro module resolution

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,7 @@
-const fs = require('fs');
-const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-const virtualStoreRoot = path.resolve(
-  fs.realpathSync(path.join(__dirname, 'node_modules', 'expo')),
-  '../../..',
-);
-
-config.watchFolders = [...new Set([...(config.watchFolders ?? []), virtualStoreRoot])];
-config.resolver.nodeModulesPaths = [
-  path.join(__dirname, 'node_modules'),
-  ...(config.resolver.nodeModulesPaths ?? []),
-];
 
 module.exports = withNativeWind(config, { input: './global.css' });
 
